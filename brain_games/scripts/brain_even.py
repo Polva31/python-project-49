73a79ed9e99b40bc8bcbe2f4a5fc12a51d757f16
Polva31 <v.pol-31@yandex.ru>
2025-12-15 21:39:30 +0300

    correct import in brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
-from brain_games.engine import run_game
+
+from brain_games.engine import run
 from brain_games.games.even import generate_round
+
 
 def main():
     description = "Answer yes if number even, else answer no"
-    run_game(generate_round, description)
+    run(generate_round, description)
+
 
 if __name__ == "__main__":
     main()
